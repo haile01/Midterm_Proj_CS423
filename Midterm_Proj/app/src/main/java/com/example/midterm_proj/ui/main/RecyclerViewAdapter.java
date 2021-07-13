f43531453df02213cf0ae29c5821704223df58ea
pythagore1123 <ledinhhai1312@gmail.com>
2021-07-13 11:46:27 +0700

    Merged /photo-fragment, minor fix dateformat

--- Midterm_Proj/app/src/main/java/com/example/midterm_proj/ui/main/RecyclerViewAdapter.java
+++ Midterm_Proj/app/src/main/java/com/example/midterm_proj/ui/main/RecyclerViewAdapter.java
@@ -28,8 +28,6 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     GridViewAdapter gridViewAdapter;
     OpenPopupHandler mOpenPopupHandler;
 
-    @SuppressLint("SimpleDateFormat")
-    DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
     public RecyclerViewAdapter(PhotosFragment mContext, List<PhotoDate> mData) {
         this.mContext = mContext;
         this.photosDate = mData;
@@ -81,34 +79,11 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     }
 
     private String getParseStringDate(Date date){
-        @SuppressLint("SimpleDateFormat") DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("E, dd MMM yyyy");
         Date currentDate = new Date();
-        String dayInWeek = "Mon";
         String currentDateString = dateFormat.format(currentDate);
-        switch (date.getDay()){
-            case 1:
-                dayInWeek = "Mon";
-                break;
-            case 2:
-                dayInWeek = "Tue";
-                break;
-            case 3:
-                dayInWeek = "Wed";
-                break;
-            case 4:
-                dayInWeek = "Thus";
-                break;
-            case 5:
-                dayInWeek = "Fri";
-                break;
-            case 6:
-                dayInWeek = "Sat";
-                break;
-            default:
-                dayInWeek = "Sun";
-                break;
-        }
-        String strDate = dayInWeek + " " + dateFormat.format(date);
+
+        String strDate = dateFormat.format(date);
 
         if (currentDateString.equals(dateFormat.format(date))) return "Today";
         else if (dateFormat.format(yesterday()).equals(dateFormat.format(date))) return "Yesterday";
