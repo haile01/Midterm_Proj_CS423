f650a63b170a12d7053ec2286b2234bfbf538218
pythagore1123 <ledinhhai1312@gmail.com>
2021-09-06 12:29:03 +0700

    Minor fix

--- Midterm_Proj/app/src/main/java/com/example/midterm_proj/MainActivity.java
+++ Midterm_Proj/app/src/main/java/com/example/midterm_proj/MainActivity.java
@@ -71,7 +71,7 @@ public class MainActivity extends AppCompatActivity implements OpenPopupHandler,
 
         ActivityCompat.requestPermissions(
                 MainActivity.this,
-                new String[] {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},
+                new String[] {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE},
                 123);
 
         initializeSectionsPager();
